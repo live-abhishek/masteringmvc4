df2be64f6e0414a3839037d675891c484b8f8534
Masterchief <live.abhishek@outlook.com>
2018-07-03 19:47:13 +0530

    Renamed HelloController to TweetController Added code to get all tweets related to a seach term Update the thymeleaf page to show all result tweets in a list

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -2,14 +2,18 @@ package masterSpringMvc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
-public class HelloController {
+public class TweetController {
 
     @Autowired
     private Twitter twitter;
@@ -17,8 +21,8 @@ public class HelloController {
     @RequestMapping("/")
     public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model){
         SearchResults searchResults = twitter.searchOperations().search(search);
-        String text = searchResults.getTweets().get(0).getText();
-        model.addAttribute("message", text);
+        List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
+        model.addAttribute("tweets", tweets);
         return "resultPage";
     }
 }
