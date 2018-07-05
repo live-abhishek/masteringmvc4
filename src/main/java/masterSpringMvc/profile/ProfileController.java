22ccff1831f39dc72cee573a0b577f5a55c7a949
Masterchief <live.abhishek@outlook.com>
2018-07-05 18:54:17 +0530

    Added BindingResult to propagate the errors to the profilePage when validation error occurs

--- src/main/java/masterSpringMvc/profile/ProfileController.java
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -2,6 +2,7 @@ package masterSpringMvc.profile;
 
 import masterSpringMvc.date.USLocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +19,10 @@ public class ProfileController {
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(@Valid ProfileForm profileForm){
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "profile/profilePage";
+        }
         System.out.println("save ok" + profileForm);
         return "redirect:/profile";
     }
